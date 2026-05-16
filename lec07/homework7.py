fd1f5121aba9aaf9c8c0e4cc2aba46359ac8c2bc
Vidushika97 <ict2019052@as.rjt.ac.lk>
2026-05-16 22:13:25 +0900

    homework7

--- lec07/homework7.py
+++ lec07/homework7.py
@@ -3,44 +3,69 @@ import numpy as np
 def major_chord(f, Fs):
     '''
     Generate a one-half-second major chord, based at frequency f, with sampling frequency Fs.
-
-    @param:
-    f (scalar): frequency of the root tone, in Hertz
-    Fs (scalar): sampling frequency, in samples/second
-
-    @return:
-    x (array): a one-half-second waveform containing the chord
-    
-    A major chord is three notes, played at the same time:
-    (1) The root tone (f)
-    (2) A major third, i.e., four semitones above f
-    (3) A major fifth, i.e., seven semitones above f
     '''
-    raise RuntimeError("You need to write this part")
+    
+    duration = 0.5
+    N = int(Fs * duration)
+
+    n = np.arange(N)
+
+    # Frequencies of major chord
+    f_root = f
+    f_third = f * (2 ** (4/12))   # major third
+    f_fifth = f * (2 ** (7/12))  # perfect fifth
+
+    # Generate tones
+    x1 = np.cos(2 * np.pi * f_root * n / Fs)
+    x2 = np.cos(2 * np.pi * f_third * n / Fs)
+    x3 = np.cos(2 * np.pi * f_fifth * n / Fs)
+
+    # Combine tones
+    x = x1 + x2 + x3
+
+    return x
+
 
 def dft_matrix(N):
     '''
     Create a DFT transform matrix, W, of size N.
-    
-    @param:
-    N (scalar): number of columns in the transform matrix
-    
-    @result:
-    W (NxN array): a matrix of dtype='complex' whose (k,n)^th element is:
-           W[k,n] = cos(2*np.pi*k*n/N) - j*sin(2*np.pi*k*n/N)
     '''
-    raise RuntimeError("You need to write this part")
+
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+
+    W = np.cos(2 * np.pi * k * n / N) - 1j * np.sin(2 * np.pi * k * n / N)
+
+    return W
+
 
 def spectral_analysis(x, Fs):
     '''
     Find the three loudest frequencies in x.
-
-    @param:
-    x (array): the waveform
-    Fs (scalar): sampling frequency (samples/second)
-
-    @return:
-    f1, f2, f3: The three loudest frequencies (in Hertz)
-      These should be sorted so f1 < f2 < f3.
     '''
-    raise RuntimeError("You need to write this part")
+
+    N = len(x)
+
+    # FFT
+    X = np.fft.fft(x)
+
+    # Magnitudes
+    magnitude = np.abs(X)
+
+    # Frequency axis
+    freqs = np.fft.fftfreq(N, d=1/Fs)
+
+    # Use only positive frequencies
+    positive = freqs >= 0
+    freqs = freqs[positive]
+    magnitude = magnitude[positive]
+
+    # Find indices of 3 largest peaks
+    indices = np.argsort(magnitude)[-3:]
+
+    # Corresponding frequencies
+    loudest_freqs = np.sort(freqs[indices])
+
+    f1, f2, f3 = loudest_freqs
+
+    return f1, f2, f3
